c16560360d273b663cf874cffd3bf326acc403cc
Thomas Vidic <thomas.vidic@rewe-digital.com>
2021-03-24 23:28:57 +0100

    translations

--- lib/trees/i18n/species_picker.i18n.dart
+++ lib/trees/i18n/species_picker.i18n.dart
@@ -9,6 +9,38 @@ extension Localization on String {
       {
         "en_us": "Please select a tree species",
         "de": "Bitte wähle eine Spezies aus",
+      } +
+      {
+        "en_us": "Please enter the informal name",
+        "de": "Bitte gib einen gemeinen Namen ein",
+      } +
+      {
+        "en_us": "Informal name",
+        "de": "Gemeiner Name",
+      } +
+      {
+        "en_us": "Please enter the latin name",
+        "de": "Bitte gib den lateinischen Namen ein",
+      } +
+      {
+        "en_us": "Latin name",
+        "de": "Lateinischer Name",
+      } +
+      {
+        "en_us": "Species already existing",
+        "de": "Es existiert bereits eine Spezies mit diesem Namen",
+      } +
+      {
+        "en_us": "Add Species",
+        "de": "Spezies hinzufügen",
+      } +
+      {
+        "en_us": "Not found. Create?",
+        "de": "Spezies nicht gefunden. Hinzufügen?",
+      } +
+      {
+        "en_us": "Save",
+        "de": "Speichern",
       });
 
   String get i18n => localize(this, _t);
